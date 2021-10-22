cbc9d1c448cc082df9d2667a631d9f9ad0075d8a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-22 05:43:48 +0300

    Authenticator create

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT:3000,
-    DB_CONNECTION_STRING: 'mongodb://localhost:27017/booking'
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/booking',
+    TOKEN_SECRET: 'secured',
+    COOKIE_NAME: 'SESSION9'
 }
---
+++ middlewares/auth.js
@@ -0,0 +1,69 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { TOKEN_SECRET, COOKIE_NAME } = require('../config')
+const userService = require('../services/userService');
+
+function init() {
+    return function (req, res, next) {
+        //TODO parse jwt
+        //attach functions to context
+
+        req.auth = {
+            async register(username, password) {
+                const token = await register(username, password);
+                res.cookie(COOKIE_NAME, token);
+            },
+            async login(username, password) {
+                const token = await register(username, password);
+                res.cookie(COOKIE_NAME, token);
+            },
+            logout() {
+                res.clearCookie(COOKIE_NAME);
+            }
+        }
+
+        next();
+    };
+}
+
+async function register(username, password) {
+    //TODO adapt parameters to project requirements
+    //extra validations
+    const existing = await userService.getUserByUsername(username);
+
+    if (existing) {
+        throw new Error('Username is taken!');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userService.createUser(username, hashedPassword);
+
+    return generateToken(user);
+
+};
+
+async function login(username, password) {
+    const user = await userService.getUserByUsername(username);
+
+    if (!user) {
+        throw new Error('No such user!');
+    }
+
+    const hasMatch = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!hasMatch) {
+        throw new Error('Incorrect password!');
+    }
+
+    return generateToken(user);
+}
+
+
+function generateToken(userData) {
+    return jwt.sign({
+        _id: user._id,
+        username: user.username
+    }, TOKEN_SECRET);
+
+}
