afaa54242dd60096ca805d0956fac6e4f5bc8ced
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-22 05:02:19 +0300

    User Model create

---
+++ models/User.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema ({
+    username: { type: String, required: true},
+    hashedPassword: { type: String, required: true}
+})
