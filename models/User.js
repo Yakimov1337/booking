944c83900b02da9464cf7e136af77b4546249eb3
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-22 05:44:06 +0300

    User service

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const schema = new Schema ({
+    //TODO adapt parameters to project requirements
     username: { type: String, required: true},
     hashedPassword: { type: String, required: true}
 })
