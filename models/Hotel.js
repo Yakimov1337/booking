818fe108c75ef1e64a910db2531d408c5b3c3024
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-26 01:42:52 +0300

    Hotel create

--- models/Hotel.js
+++ models/Hotel.js
@@ -2,12 +2,12 @@ const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
     //TODO adapt parameters to project requirements
-    name: {type:String, required:true},
-    city: {type:String, required:true},
-    imageUrl: {type:String, required:true},
-    rooms: {type:String, required:true, min:1, max:100},
-    bookedBy:[{type: Schema.Types.ObjectId, ref: 'User'}],
-    owner: {type: Schema.Types.ObjectId, ref: 'User', required:true}
+    name: { type: String, required: [true, "All fields are required"], minLength: 4 },
+    city: { type: String, required: [true, "All fields are required"], minLength: 3 },
+    imageUrl: { type: String, required: [true, "All fields are required"] , match: [/^http?/, "Image must be valid URL"]},
+    rooms: { type: String, required: [true, "All fields are required"], min: 1, max: 100 },
+    bookedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 module.exports = model('Hotel', schema);
