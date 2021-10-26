1d0db94c30a684913862fbf631cb8c74bf332cd7
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-27 00:27:15 +0300

    Hotel rooms fix

--- models/Hotel.js
+++ models/Hotel.js
@@ -5,7 +5,7 @@ const schema = new Schema({
     name: { type: String, required: [true, "All fields are required"], minLength: 4 },
     city: { type: String, required: [true, "All fields are required"], minLength: 3 },
     imageUrl: { type: String, required: [true, "All fields are required"] , match: [/^http?/, "Image must be valid URL"]},
-    rooms: { type: String, required: [true, "All fields are required"], min: 1, max: 100 },
+    rooms: { type: Number, required: [true, "All fields are required"], min: 1, max: 100 },
     bookedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     owner: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 })
